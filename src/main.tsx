8a226b604d11c26d845020afbec2d0d25059d54a
giovanaup-debug <giovapereira@gmail.com>
2026-06-08 20:10:40 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from './router'
-import '../styles.css' // <-- Aponta para o arquivo de estilos global na raiz
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
