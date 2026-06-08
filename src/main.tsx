9f18c5231d8f5fb5b2f8c17b9eaf46fd2dd58e8e
giovanaup-debug <giovapereira@gmail.com>
2026-06-08 19:48:25 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
-import { hydrateRoot } from 'react-dom/client'
-import { StartClient } from '@tanstack/start/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from '@tanstack/react-router'
 import { router } from './router'
 import './styles.css'
 
-// Acorda o ecossistema do TanStack conectado ao HTML
-hydrateRoot(document.getElementById('root')!, <StartClient router={router} />)
+// Inicializa o app renderizando o provedor de rotas diretamente na div raiz
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
