d476ef218eef2c3bf6937262fef6de510f53d188
giovanaup-debug <giovapereira@gmail.com>
2026-06-08 19:02:04 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,1 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { resolve } from "path";
-
-export default defineConfig({
-  base: "./", // Garante caminhos relativos perfeitos
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
-});
+export { tanstackViteConfig as default } from "@lovable.dev/vite-tanstack-config";
