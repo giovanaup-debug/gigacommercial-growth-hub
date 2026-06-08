af480635c734178548565bcbfb082790b00302ac
giovanaup-debug <giovapereira@gmail.com>
2026-06-08 19:20:16 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,4 @@
-export { tanstackViteConfig as default } from "@lovable.dev/vite-tanstack-config";
+import lovablePkg from "@lovable.dev/vite-tanstack-config";
+
+// Exporta o pacote padrão que o Vinxi e o TanStack precisam para mapear as pastas
+export default lovablePkg;
