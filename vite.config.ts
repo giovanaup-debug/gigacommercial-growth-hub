b2dce618fba9ad8cd22d4fefb899b8ae3cef4198
giovanaup-debug <giovapereira@gmail.com>
2026-06-08 23:41:52 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // <--- Este plugin é o que ativa as cores, fontes e o grid na Render
+    tailwindcss(),
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': '/src', // Sintaxe moderna para ESM que elimina o uso de __dirname
     },
   },
 });
