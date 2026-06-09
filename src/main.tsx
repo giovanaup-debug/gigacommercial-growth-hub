e9ea793779586821825ca7cd51a93a031c0bfa1a
giovanaup-debug <giovapereira@gmail.com>
2026-06-08 22:15:44 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from './router'
+import './styles.css' // O Tailwind v4 será injetado por aqui de forma nativa
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
